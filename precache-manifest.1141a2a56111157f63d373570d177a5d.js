11f7cd4363efd76a5c917cd240a02be6b98b29ce
c11g <renimo@hanmail.net>
2020-09-29 13:25:17 +0900

    Updates

---
+++ precache-manifest.1141a2a56111157f63d373570d177a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d284dde5380a2c10569a01ca86abbef",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "40b34a01f79cf226db4b",
+    "url": "/nwitter/static/css/main.46ec6bf7.chunk.css"
+  },
+  {
+    "revision": "381b132fade0ad2957bb",
+    "url": "/nwitter/static/js/2.0a6967d7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.0a6967d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b34a01f79cf226db4b",
+    "url": "/nwitter/static/js/main.3141c557.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
